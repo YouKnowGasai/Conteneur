6440887b2578adeabd0ec2c564fb7c89f3fb42e0
Chaignon Valentin <80058928+YouKnowGasai@users.noreply.github.com>
2024-03-12 01:48:40 +0100

    Add files via upload

---
+++ Conteneur/db/init.sql
@@ -0,0 +1,14 @@
+-- db/init.sql
+
+CREATE DATABASE IF NOT EXISTS my_database;
+USE my_database;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (username, email) VALUES
+('user1', 'user1@example.com'),
+('user2', 'user2@example.com');
